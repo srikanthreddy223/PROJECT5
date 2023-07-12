8f709ad74f10f8492009c4f531e7ea8b513203a5
srikanthreddy223 <139108379+srikanthreddy223@users.noreply.github.com>
2023-07-12 18:56:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var image =document.getElementById("image");
+function bulb_on(){
+    image.src = "./images/bulb-on-off.jpg";
+
+}
+function bulb_off(){
+    image.src = "./images/off image.jpg";
+}
